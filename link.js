d78a9e695116db5d79f8f78608577f8748db82ed
denwtw <denard.howard@hgs.hiddengeniusproject.org>
2025-08-04 13:21:58 -0400

    Add files via upload

---
+++ link.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const box = document.getElementById("expandableBox");
+  const link = "https://fortnitetracker.com/profile/all/srw%20grim%c7%83";
+
+  box.addEventListener("click", () => {
+    window.open(link, "_blank");
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const box = document.getElementById("expandableBox");
+  const link = "https://www.youtube.com/@grim5x";
+
+  box.addEventListener("click", () => {
+    window.open(link, "_blank");
+  });
+});
